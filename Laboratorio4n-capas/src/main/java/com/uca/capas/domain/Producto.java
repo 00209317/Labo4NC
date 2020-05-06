2978e316887338e4d713322dbd8a494ca9ca98a8
Daniel Hernandez <00209317@uca.edu.sv>
2020-05-06 13:19:30 -0600

    Terminado antes del robot xD

--- Laboratorio4n-capas/src/main/java/com/uca/capas/domain/Producto.java
+++ Laboratorio4n-capas/src/main/java/com/uca/capas/domain/Producto.java
@@ -1,11 +1,30 @@
 package com.uca.capas.domain;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Producto {
 
+	@Size(message = "El nombre no debe tener más de 15 caracteres", max=15)
+	@NotEmpty(message = "Este campo no puede estar vacío")
 	private String nombre;
+	
+	@Size(message = "El nombre no debe tener más de 30 caracteres", max=30)
+	@NotEmpty(message = "Este campo no puede estar vacío")
 	private String descripcion;
+	
+	@NotNull(message = "El campo no puede estar vacío")
+	@Min(value = 0, message = "El precio no debe ser negativo")
 	private Double precio;
+	
+	@NotEmpty(message = "El campo no puede estar vacío")
+	@Pattern(regexp = "^([0-2][0-9]|3[0-1])(\\/|-)(0[1-9]|1[0-2])\\2(\\d{4})$", message = "el formato debe se dd/mm/aaaa")
 	private String vencimiento;
+	
+	
 	public String getNombre() {
 		return nombre;
 	}
