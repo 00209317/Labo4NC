70a07339423e467f7030781c7d39a6b0eaa5d2c9
Daniel Hernandez <00209317@uca.edu.sv>
2020-05-06 12:26:11 -0600

    Producto y MainController modificados

--- Laboratorio4n-capas/src/main/java/com/uca/capas/controller/MainController.java
+++ Laboratorio4n-capas/src/main/java/com/uca/capas/controller/MainController.java
@@ -1,5 +1,19 @@
 package com.uca.capas.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class MainController {
 
+	@RequestMapping("/producto")
+	public ModelAndView initMain() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+	}
+	
+	
+	
 }
---
+++ Laboratorio4n-capas/src/main/java/com/uca/capas/domain/Producto.java
@@ -0,0 +1,34 @@
+package com.uca.capas.domain;
+
+public class Producto {
+
+	private String nombre;
+	private String descripcion;
+	private Double precio;
+	private String vencimiento;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public String getVencimiento() {
+		return vencimiento;
+	}
+	public void setVencimiento(String vencimiento) {
+		this.vencimiento = vencimiento;
+	}
+	
+}
