cc0e26ae479f6ab0aaa78e856f57ebbd6d72d7d3
Daniel Hernandez <00209317@uca.edu.sv>
2020-05-06 20:49:31 -0600

    Robot y MainController

--- Laboratorio4n-capas/src/main/java/com/uca/capas/controller/MainController.java
+++ Laboratorio4n-capas/src/main/java/com/uca/capas/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.uca.capas.domain.Producto;
+import com.uca.capas.domain.Robot;
 
 @Controller
 public class MainController {
@@ -29,6 +30,19 @@ public class MainController {
 			mav.setViewName("index");
 		}
 		else {
+			Robot robot = new Robot();
+			mav.addObject("robot", robot);
+			mav.setViewName("robot");
+		}
+		return mav;
+	}
+	
+	@RequestMapping("/seguridad")
+	public ModelAndView formSeguridad(@Valid @ModelAttribute Robot robot, BindingResult result) {
+		ModelAndView mav = new ModelAndView();
+		if(result.hasErrors()) {
+			mav.setViewName("robot");
+		}else {
 			mav.setViewName("exito");
 		}
 		return mav;
